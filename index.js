b7cf4cc20f1dcb7f5a2ec28eb72f7e0a95a841ac
Naima Hassan <naima.hassan434@gmail.com>
2022-02-22 23:43:34 +0000

    POST request with Body Parser

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 //route declare
 app.get("/", (req, res) => {
@@ -7,6 +14,13 @@ app.get("/", (req, res) => {
 });
 app.get("/about", (req, res) => {
   res.json({ title: "Hi JSON" });
+});
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  console.log(req.body.email);
+  console.log(req.body.password);
+
+  res.send("Login Successful");
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
